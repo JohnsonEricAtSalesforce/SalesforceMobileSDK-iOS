3480b7606284f9c3180e1bd678b54aa0de22a98f
Eric C. Johnson <Johnson.Eric@Salesforce.com>
2026-05-20 10:22:22 -0600

    Convert SFNetworkTests + SalesforceOAuthUnitTests to Swift (unblock Item 3)
    - Convert SFNetworkTests.m (158 lines) to SFNetworkTests.swift
    - Convert SalesforceOAuthUnitTests.m (527 lines) to SalesforceOAuthUnitTests.swift
    - Merge SalesforceOAuthUnitTestsCoordinatorDelegate into the Swift test file
    - Update bridging header with SFOAuthKeychainCredentials Testing category
    - Update project.pbxproj to reference new Swift files
    - Remove old .m and .h files

--- libs/SalesforceSDKCore/SalesforceSDKCoreTests/SalesforceSDKCoreTests-Bridging-Header.h
+++ libs/SalesforceSDKCore/SalesforceSDKCoreTests/SalesforceSDKCoreTests-Bridging-Header.h
@@ -9,3 +9,10 @@
 #import "SFOAuthCoordinator+Internal.h"
 #import "SFUserAccountManager+Internal.h"
 #import "SFOAuthCredentials+Internal.h"
+#import "SFOAuthKeychainCredentials.h"
+
+// Expose internal keychain token methods for testing
+@interface SFOAuthKeychainCredentials (Testing)
+- (NSString * _Nullable)decryptedTokenForService:(NSString * _Nonnull)service;
+- (void)encryptToken:(NSString * _Nonnull)token forService:(NSString * _Nonnull)service;
+@end
